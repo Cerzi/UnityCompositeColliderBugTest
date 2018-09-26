b15a4766dfc4d0fd736fe00bd852f706d021b292
Cerzi <bpateman@gmail.com>
2018-09-26 14:56:38 +0100

    first commit

---
+++ Assets/Scripts/DumbMove.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DumbMove : MonoBehaviour
+{
+    public float speed = 2;
+    private void Update()
+    {
+        transform.position = new Vector2
+            (transform.position.x + (speed * Time.deltaTime), 
+            transform.position.y);
+    }
+}
---
+++ Assets/Scripts/TilemapColliderResolve.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TilemapColliderResolve : MonoBehaviour
+{
+    public Text debugText;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        debugText.text = "TRIGGER ENTER";
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        debugText.text = "TRIGGER EXIT";
+    }
+}
